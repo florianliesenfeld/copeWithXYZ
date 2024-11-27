1ee4bc1028a0313c7041c56ea1b0f5347090dda2
Florian Liesenfeld <florian.liesenfeld@gmx.de>
2024-11-27 09:16:46 +0100

    Add placeholder site

---
+++ placeholder/js/placeholder.js
@@ -0,0 +1,22 @@
+let conditions = [  "xyz","allergies","intolerances","mentalLoad","chronicDisease",
+                    "alzheimer","care","MECFS","POTS","MCAD","longCovid","postCovid","life"];
+const urlEnding = document.querySelector(".url__ending");
+const intervalID = window.setInterval(changeEnding,2000);
+let index = 0;
+let i=0;
+let contidionsToUse = [];
+
+function changeEnding() {
+    if(contidionsToUse.length<1) {
+        contidionsToUse = [...conditions];
+    }
+    if(i%2===0){
+        urlEnding.textContent = contidionsToUse[index];
+        contidionsToUse.splice(index,1);
+        index = Math.floor(Math.random()*contidionsToUse.length);
+    } else {
+        // urlEnding.classList.toggle("url__ending--middle");
+    }
+    urlEnding.classList.toggle("url__ending--up");
+    i++;
+}
