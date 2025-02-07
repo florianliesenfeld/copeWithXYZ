32357e8fad45f1b7ac5bf06abf03c09ee0c9da48
Florian Liesenfeld <florian.liesenfeld@gmx.de>
2025-02-07 12:33:21 +0100

    Add react test

--- placeholder/js/placeholder.js
+++ placeholder/js/placeholder.js
@@ -1,19 +1,19 @@
-let conditions = [  "xyz","allergies","intolerances","mentalLoad","chronicDisease",
+const conditions = [  "xyz","allergies","intolerances","mentalLoad","chronicDisease",
                     "alzheimer","care","MECFS","POTS","MCAD","longCovid","postCovid","life"];
 const urlEnding = document.querySelector(".url__ending");
 const intervalID = window.setInterval(changeEnding,2000);
 let index = 0;
 let i=0;
-let contidionsToUse = [];
+let conditionToUse = [];
 
 function changeEnding() {
-    if(contidionsToUse.length<1) {
-        contidionsToUse = [...conditions];
+    if(conditionToUse.length<1) {
+        conditionToUse = [...conditions];
     }
     if(i%2===0){
-        urlEnding.textContent = contidionsToUse[index];
-        contidionsToUse.splice(index,1);
-        index = Math.floor(Math.random()*contidionsToUse.length);
+        urlEnding.textContent = conditionToUse[index];
+        conditionToUse.splice(index,1);
+        index = Math.floor(Math.random()*conditionToUse.length);
     } else {
         // urlEnding.classList.toggle("url__ending--middle");
     }
